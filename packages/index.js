0d427863653fa36f7bfabc88b5bbb1b564fbdb94
xiaosong.jiang <xiaosong.jiang@geely.com>
2019-06-11 14:32:50 +0800

    feat: add lib canvas2image

--- packages/index.js
+++ packages/index.js
@@ -1,10 +1,13 @@
-/*水印功能*/
+/* 水印功能 */
 import Watermark from './src/watermark'
 
-/*保存至图片*/
+/* 保存至图片 */
 import Canvas2some from './src/canvas2some'
+
+import Canvas2image from './src/canvas2image'
 
 export {
 	Watermark,
-	Canvas2some
+	Canvas2some,
+	Canvas2image
 }
